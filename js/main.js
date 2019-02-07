e707c995847039d8301c2b24e4c7351ec15f8ff0
Daniil <shubonator7@gmail.com>
2019-02-07 12:59:19 +0300

    Menubtn

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
 $( ".menu-btn, .menu-box-btn" ).click(function() {
-    $( ".menu" ).toggleClass("menu-visible");
-    $( ".menu-box-content" ).toggleClass("menu-box-content-visible");
-    $( ".menu-box-content-1" ).toggleClass("visible1");
-    $( ".menu-box-content-2" ).toggleClass("visible2");
-    $( ".menu-box-content-3" ).toggleClass("visible3");
-    $( ".menu-box-content-4" ).toggleClass("visible4");
-    
-  });
+    $( ".menu" ).toggleClass("menu-visible");    
+});
